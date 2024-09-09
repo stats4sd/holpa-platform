cef3c5001227beebd997c71a7c7b1602a9b596ef
Dave Mills <d.e.mills@stats4sd.org>
2024-09-09 11:30:49 +0100

    add test seeder and hasRoles to user

---
+++ database/seeders/Test/TestUserSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders\Test;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+
+class TestUserSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $superAdmin = Role::create(['name' => 'Super Admin']);
+
+        $user = User::create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+            'password' => bcrypt('password'),
+        ]);
+
+        $admin = User::create([
+            'name' => 'Test Admin',
+            'email' => 'admin@example.com',
+            'password' => bcrypt('password'),
+        ]);
+
+        $admin->assignRole('Super Admin');
+    }
+}
