f6e3f7fdb3a17f91d927791f8bf59231627b1f37
Dan Tang <dan@stats4sd.org>
2024-10-07 15:32:58 +0100

    add app panel ODK Form Management page

--- app/Models/Team.php
+++ app/Models/Team.php
@@ -9,13 +9,50 @@ use Filament\Notifications\Notification;
 use App\Mail\TeamManagement\InviteMember;
 use App\Models\TeamManagement\TeamInvite;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
+use Stats4sd\FilamentOdkLink\Services\OdkLinkService;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
-class Team extends Model
+class Team extends \Stats4sd\FilamentOdkLink\Models\TeamManagement\Team
 {
     protected $table = 'teams';
 
     protected $guarded = ['id'];
+
+
+    // TODO: I think this overrides the booted method on HasXlsForms - ideally we wouldn't need to copy the package stuff here...
+    protected static function booted(): void
+    {
+
+        // when the model is created; automatically create an associated project on ODK Central and a top location level;
+        static::created(static function ($owner) {
+
+            // check if we are in local-only (no-ODK link) mode
+            $odkLinkService = app()->make(OdkLinkService::class);
+            if (config('filament-odk-link.odk.url') !== null && config('filament-odk-link.odk.url') !== '') {
+                $owner->createLinkedOdkProject($odkLinkService, $owner);
+            }
+
+            // create empty interpretation entries for the team:
+            // TODO: this probably is not great, and we should not require a bunch of empty entries!
+
+            // Below are tape-data-system specific business logic, HOPLA may have something similar.
+            // Temporary keep it for reference first. We can remove them after confirming we do not need them.
+
+            /*
+            $interpretations = CaetIndex::all()->map(fn ($index) => [
+               'owner_id' => $owner->id,
+               'owner_type' => static::class,
+               'caet_index_id' => $index->id,
+               'interpretation' => '',
+           ])->toArray();
+
+           $owner->caetInterpretations()->createMany($interpretations);
+
+           $owner->locationLevels()->create(['name' => 'Top level (rename)', 'has_farms' => 0, 'top_level' => 1, 'slug' =>'site-level']);
+           */
+        });
+    }
 
     // ****** TEAM MANAGEMENT STUFF ******
 
