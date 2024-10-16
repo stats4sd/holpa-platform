fdf7f8663666630c05fcbc4eb29da5daddf85169
Dan Tang <dan@stats4sd.org>
2024-10-16 14:46:12 +0100

    redirect user to app panel login page when user logout from admin panel or program admin panel

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,17 @@ Route::get('programregister', Programregister::class)
     ->name('filament.app.programregister')
     ->middleware('signed');
 
+// redirect user from root path to app panel login page
 Route::get('/', function () {
     return redirect('app');
 });
+
+// when user logout from program admin panel, redirect user to app panel login pager
+Route::get('/program/login', function () {
+    return redirect('app');
+})->name('filament.program.auth.login');
+
+// when user logout from admin panel, redirect user to app panel login pager
+Route::get('/admin/login', function () {
+    return redirect('app');
+})->name('filament.admin.auth.login');
