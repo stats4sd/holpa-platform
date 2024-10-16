da389cc747b4a396c5b02ecf3845bf0e279d5a1c
Dan Tang <dan@stats4sd.org>
2024-10-16 11:07:47 +0100

    WIP: add My Program item in program admin panel sidebar

---
+++ app/Http/Middleware/AddProgramAdminMenuItems.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Filament\Facades\Filament;
+use Filament\Navigation\MenuItem;
+use Symfony\Component\HttpFoundation\Response;
+
+class AddProgramAdminMenuItems
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        logger("AddProgramAdminMenuItems.handle()...");
+
+        logger('users.latest_program_id: ' . auth()->user()->latest_program_id);
+
+        filament()->getCurrentPanel()->userMenuItems([
+            MenuItem::make()
+                ->label('Profile')
+                ->url(url('/program/1/programs/1')),
+        ]);
+
+        return $next($request);
+    }
+}
--- app/Providers/Filament/ProgramPanelProvider.php
+++ app/Providers/Filament/ProgramPanelProvider.php
@@ -8,12 +8,14 @@ use Filament\Widgets;
 use App\Models\Program;
 use Filament\PanelProvider;
 use Filament\Support\Colors\Color;
+use Illuminate\Support\Facades\Auth;
 use Filament\Navigation\NavigationItem;
 use Filament\Http\Middleware\Authenticate;
 use App\Filament\App\Pages\RegisterProgram;
 use App\Http\Middleware\CheckIfProgramAdmin;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\Cookie\Middleware\EncryptCookies;
+use App\Http\Middleware\AddProgramAdminMenuItems;
 use App\Http\Middleware\SetLatestProgramMiddleware;
 use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\AuthenticateSession;
@@ -61,15 +63,31 @@ class ProgramPanelProvider extends PanelProvider
                 SubstituteBindings::class,
                 DisableBladeIconComponents::class,
                 DispatchServingFilamentEvent::class,
+
+                // TODO: add menu item via middleware
+                AddProgramAdminMenuItems::class,
             ])
             ->authMiddleware([
                 Authenticate::class,
                 CheckIfProgramAdmin::class,
-            ])->navigationItems([
+            ])
+            ->navigationItems([
                 NavigationItem::make()
                     ->label(__('Return to Front end'))
                     ->icon('heroicon-o-home')
-                    ->url(url('/app'))
+                    ->url(url('/app')),
+
+                // TODO: create a "My Program" item in sidebar, link to the selected program view page directly
+                // e.g. http://holpa-platform.test/program/1/programs/1
+                //
+                // issue: cannot get logged in user at this point, auth()->user() returns null...
+                // auth()->user()->latestProgram()->id
+
+                // NavigationItem::make()
+                //     ->label(__('My Program'))
+                //     ->icon('heroicon-o-home')
+                //     ->group('My group')
+                //     ->url(url('/program/1/programs/1')),
             ])
             ->darkMode(false)
             ->plugins([]);
