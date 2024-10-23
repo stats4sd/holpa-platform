14b940db7a6c538b9d3290a05dcc6378b56ecf8a
Dan Tang <dan@stats4sd.org>
2024-10-23 14:44:48 +0100

    submodule update - WIP: move routes from main repo to submodule

--- routes/web.php
+++ routes/web.php
@@ -6,20 +6,20 @@ use Stats4sd\FilamentTeamManagement\Filament\App\Pages\Register;
 use Stats4sd\FilamentTeamManagement\Filament\App\Pages\Roleregister;
 use Stats4sd\FilamentTeamManagement\Filament\App\Pages\Programregister;
 
-// user registration form for team-invites
-Route::get('register', Register::class)
-    ->name('filament.app.register')
-    ->middleware('signed');
+// // user registration form for team-invites
+// Route::get('register', Register::class)
+//     ->name('filament.app.register')
+//     ->middleware('signed');
 
-// user registration form for role-invites
-Route::get('roleregister', Roleregister::class)
-    ->name('filament.app.roleregister')
-    ->middleware('signed');
+// // user registration form for role-invites
+// Route::get('roleregister', Roleregister::class)
+//     ->name('filament.app.roleregister')
+//     ->middleware('signed');
 
-// user registration form for program-invites
-Route::get('programregister', Programregister::class)
-    ->name('filament.app.programregister')
-    ->middleware('signed');
+// // user registration form for program-invites
+// Route::get('programregister', Programregister::class)
+//     ->name('filament.app.programregister')
+//     ->middleware('signed');
 
 // redirect user from root path to app panel login page
 Route::get('/', function () {
