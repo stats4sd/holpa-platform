58b2bd2ac4e205f281f40375e16202b994b1a5ab
Ciara <ciara@stats4sd.org>
2024-11-13 14:15:33 +0000

    adds component to page blade

--- resources/views/filament/app/pages/pilot.blade.php
+++ resources/views/filament/app/pages/pilot.blade.php
@@ -1,5 +1,5 @@
-<x-filament-panels::page>
-
-Coming soon
-
-</x-filament-panels::page>
+@livewire('page-header-with-instructions', [
+    'heading' => 'Localisation: Pilot',
+    'instructions' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ac venenatis elit. Vivamus non urna ac turpis hendrerit tincidunt ut eget risus. Curabitur sagittis, ex a consectetur convallis, libero nisi efficitur sapien, non eleifend enim lectus vel leo. Morbi tincidunt libero ut nunc scelerisque, eget fringilla nulla volutpat. Aliquam feugiat massa sit amet arcu convallis, et iaculis ligula facilisis. Etiam accumsan magna et ipsum facilisis, at malesuada nulla ornare.',
+    'videoUrl' => 'https://www.youtube.com/embed/VIDEO_ID'
+])
