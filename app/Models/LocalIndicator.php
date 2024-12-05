4bdde0f5be84550900366e82f2529d6c87bfa608
Ciara <ciara@stats4sd.org>
2024-12-05 11:52:36 +0000

    adds HasFactory to model

--- app/Models/LocalIndicator.php
+++ app/Models/LocalIndicator.php
@@ -4,9 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class LocalIndicator extends Model
 {
+    use HasFactory;
+    
     protected $table = 'local_indicators';
 
     protected $guarded = ['id'];
