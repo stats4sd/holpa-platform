fe372ed144092d0de9e120e1df20bd8cd244fcd9
Ciara <ciara@stats4sd.org>
2024-12-06 17:06:09 +0000

    adds cursor pointer

--- resources/views/filament/app/pages/lisp-indicators.blade.php
+++ resources/views/filament/app/pages/lisp-indicators.blade.php
@@ -8,19 +8,19 @@
 
     <!-- Tabs -->
     <div class="grid grid-cols-3 gap-6">
-        <div wire:click="setActiveTab('local')" class="{{ $activeTab === 'local' ? 'bg-gray-200' : '' }} rounded-3xl">
+        <div wire:click="setActiveTab('local')" class="{{ $activeTab === 'local' ? 'bg-gray-200' : '' }} rounded-3xl cursor-pointer">
             <livewire:rounded-square
                 heading="UPLOAD LOCAL INDICATORS"
                 description="Upload the local indicators you identified in the LISP workshop."
             />
         </div>
-        <div wire:click="setActiveTab('match')" class="{{ $activeTab === 'match' ? 'bg-gray-200' : '' }} rounded-3xl">
+        <div wire:click="setActiveTab('match')" class="{{ $activeTab === 'match' ? 'bg-gray-200' : '' }} rounded-3xl cursor-pointer">
             <livewire:rounded-square
                 heading="MATCH WITH EXISTING GLOBAL INDICATORS"
                 description="Browse the list of indicators already available in the HOLPA global survey, and match them to your identified local indicators."
             />
         </div>
-        <div wire:click="setActiveTab('custom')" class="{{ $activeTab === 'custom' ? 'bg-gray-200' : '' }} rounded-3xl">
+        <div wire:click="setActiveTab('custom')" class="{{ $activeTab === 'custom' ? 'bg-gray-200' : '' }} rounded-3xl cursor-pointer">
             <livewire:rounded-square
                 heading="ADD CUSTOM INDICATORS"
                 description="If your local indicators do not already exist in the global survey, you can add them as custom indicators."
