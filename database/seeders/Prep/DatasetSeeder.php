6a69790dd3471a29bc57d45b80c3d9b2c4b27eeb
Dave Mills <d.e.mills@stats4sd.org>
2024-12-09 10:44:30 +0000

    link datasets to parent 'Farm Survey Data'

--- database/seeders/Prep/DatasetSeeder.php
+++ database/seeders/Prep/DatasetSeeder.php
@@ -19,17 +19,17 @@ class DatasetSeeder extends Seeder
 
         \DB::table('datasets')->delete();
 
-        Dataset::create(['name' => 'Farm Survey Data', 'parent_id' => NULL, 'primary_key' => 'id']);
-        Dataset::create(['name' => 'Products', 'parent_id' => NULL, 'primary_key' => 'id']);
-        Dataset::create(['name' => 'Permanent Workers', 'parent_id' => NULL, 'primary_key' => 'id']);
-        Dataset::create(['name' => 'Seasonal Workser in a Season', 'parent_id' => NULL, 'primary_key' => 'id']);
-        Dataset::create(['name' => 'Ecological Practices', 'parent_id' => NULL, 'primary_key' => 'id']);
-        Dataset::create(['name' => 'Crops', 'parent_id' => NULL, 'primary_key' => 'id']);
-        $livestock = Dataset::create(['name' => 'Livestock', 'parent_id' => NULL, 'primary_key' => 'id']);
+        $farmSurveyDataset = Dataset::create(['name' => 'Farm Survey Data', 'parent_id' => NULL, 'primary_key' => 'id']);
+        Dataset::create(['name' => 'Products', 'parent_id' => $farmSurveyDataset->id, 'primary_key' => 'id']);
+        Dataset::create(['name' => 'Permanent Workers', 'parent_id' => $farmSurveyDataset->id, 'primary_key' => 'id']);
+        Dataset::create(['name' => 'Seasonal Workser in a Season', 'parent_id' => $farmSurveyDataset->id, 'primary_key' => 'id']);
+        Dataset::create(['name' => 'Ecological Practices', 'parent_id' => $farmSurveyDataset->id, 'primary_key' => 'id']);
+        Dataset::create(['name' => 'Crops', 'parent_id' => $farmSurveyDataset->id, 'primary_key' => 'id']);
+        $livestock = Dataset::create(['name' => 'Livestock', 'parent_id' => $farmSurveyDataset->id, 'primary_key' => 'id']);
         Dataset::create(['name' => 'Livestock Uses', 'parent_id' => $livestock->id, 'primary_key' => 'id']);
-        $fish = Dataset::create(['name' => 'Fish', 'parent_id' => NULL, 'primary_key' => 'id']);
+        $fish = Dataset::create(['name' => 'Fish', 'parent_id' => $farmSurveyDataset->id, 'primary_key' => 'id']);
         Dataset::create(['name' => 'Fish Uses', 'parent_id' => $fish->id, 'primary_key' => 'id']);
-        Dataset::create(['name' => 'Growing Seasons (Irrigation)', 'parent_id' => NULL, 'primary_key' => 'id']);
-        Dataset::create(['name' => 'Sites', 'parent_id' => NULL, 'primary_key' => 'id']);
+        Dataset::create(['name' => 'Growing Seasons (Irrigation)', 'parent_id' => $farmSurveyDataset->id, 'primary_key' => 'id']);
+        Dataset::create(['name' => 'Sites', 'parent_id' => $farmSurveyDataset->id, 'primary_key' => 'id']);
     }
 }
