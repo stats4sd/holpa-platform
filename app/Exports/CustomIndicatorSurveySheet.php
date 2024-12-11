c2436c3699a8b6c3c4a1de52c7bc18ac09700c13
Ciara <ciara@stats4sd.org>
2024-12-11 12:29:34 +0000

    adds choices sheet to export

---
+++ app/Exports/CustomIndicatorSurveySheet.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Exports;
+
+use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithTitle;
+use Maatwebsite\Excel\Concerns\WithStyles;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
+use PhpOffice\PhpSpreadsheet\Style\Fill;
+
+class CustomIndicatorSurveySheet implements FromCollection, WithHeadings, WithTitle, WithStyles, ShouldAutoSize
+{
+    protected $team;
+
+    public function __construct($team)
+    {
+        $this->team = $team;
+    }
+
+    public function collection()
+    {
+        return collect($this->team->localIndicators()->where('is_custom', 1)->get())->map(function ($indicator) {
+            return [
+                'indicator_id' => $indicator->id,
+                'indicator' => $indicator->name,
+            ];
+        });
+    }
+
+    public function title(): string
+    {
+        return 'survey';
+    }
+
+    public function headings(): array
+    {
+        $locales = $this->team->locales()->with('language')->get();
+
+        $headings = [
+            'indicator ID',
+            'indicator',
+            'type',
+            'name',
+        ];
+
+        foreach ($locales as $locale) {
+            $headings[] = "label::$locale->odk_label";
+            $headings[] = "hint::$locale->odk_label";
+        }
+
+        $headings = array_merge($headings, [
+            'required',
+        ]);
+
+        foreach ($locales as $locale) {
+            $headings[] = "required_message::$locale->odk_label";
+        }
+
+        $headings = array_merge($headings, [
+            'calculation',
+            'relevant',
+            'appearance',
+            'constraint',
+        ]);
+
+        foreach ($locales as $locale) {
+            $headings[] = "constraint_message::$locale->odk_label";
+        }
+
+        $headings = array_merge($headings, [
+            'choice_filter',
+            'repeat_count',
+            'default',
+        ]);
+
+        foreach ($locales as $locale) {
+            $headings[] = "media::image::$locale->odk_label";
+        }
+
+        return $headings;
+    }
+
+    public function styles(Worksheet $sheet)
+    {
+        $wrap = [
+            'alignment' => [
+                'wrapText' => true
+            ],
+        ];
+
+        $h1 = [
+            'font' => ['bold' => true, 'color' => ['argb' => 'FFFFFF']],
+            'fill' => [
+                'fillType' => Fill::FILL_SOLID,
+                'startColor' => ['argb' => '5FA35A'],
+            ],
+        ];
+
+        return [
+            1 => $h1
+        ];
+    }
+}
