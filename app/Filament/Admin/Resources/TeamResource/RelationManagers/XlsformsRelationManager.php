5ee4edc3edd6da3e0478ebce445d20b056824a85
Dave Mills <d.e.mills@stats4sd.org>
2024-12-12 11:21:30 +0000

    generate xlsform file on creation + add stylings

--- app/Filament/Admin/Resources/TeamResource/RelationManagers/XlsformsRelationManager.php
+++ app/Filament/Admin/Resources/TeamResource/RelationManagers/XlsformsRelationManager.php
@@ -5,8 +5,11 @@ namespace App\Filament\Admin\Resources\TeamResource\RelationManagers;
 use App\Exports\XlsformExport\XlsformWorkbookExport;
 use App\Models\Xlsforms\Xlsform;
 use Filament\Tables\Actions\Action;
+use Filament\Tables\Actions\CreateAction;
 use Filament\Tables\Table;
 use Maatwebsite\Excel\Facades\Excel;
+use Stats4sd\FilamentOdkLink\Jobs\UpdateXlsformTitleInFile;
+use Stats4sd\FilamentOdkLink\Services\OdkLinkService;
 
 class XlsformsRelationManager extends \Stats4sd\FilamentOdkLink\Filament\Resources\TeamResource\RelationManagers\XlsformsRelationManager
 {
@@ -23,6 +26,17 @@ class XlsformsRelationManager extends \Stats4sd\FilamentOdkLink\Filament\Resourc
                     return Excel::download((new XlsformWorkbookExport($record)), 'xlsform.xlsx');
 
                 })
+            ])
+            ->headerActions([
+                CreateAction::make()
+                    ->label('Add Xlsform to Team')
+                    ->after(function (Xlsform $record) {
+
+                        $odkLinkService = app()->make(OdkLinkService::class);
+
+                        $record->refresh();
+                        $record->deployDraft($odkLinkService);
+                    }),
             ]);
     }
 }
