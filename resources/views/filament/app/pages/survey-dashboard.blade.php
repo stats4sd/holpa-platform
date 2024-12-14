572e593f4fe43abffeff440adf3a717541c0f74d
Dave Mills <d.e.mills@stats4sd.org>
2024-12-14 16:39:56 +0000

    add info-panels as topbar-after sections

--- resources/views/filament/app/pages/survey-dashboard.blade.php
+++ resources/views/filament/app/pages/survey-dashboard.blade.php
@@ -13,34 +13,6 @@ use App\Filament\App\Pages\DataAnalysis;
 <x-filament-panels::page>
     <div>
 
-        <!-- Header Section -->
-        <div class="bg-white shadow-md pt-8 px-4">
-
-            <!-- Heading -->
-            <h1 class="text-green font-bold text-left text-2xl mb-8">HOLPA Survey Dashboard</h1>
-
-            <!-- Content -->
-            <div class="grid grid-cols-1 md:grid-cols-2 gap-8 mb-16">
-
-                <!-- Video -->
-                <div class="p-4 rounded-md">
-                    <iframe class="w-full h-80 rounded-md" src="https://www.youtube.com/embed/TODO_ADD_VIDEO_ID" frameborder="0" allowfullscreen></iframe>
-                </div>
-
-                <!-- Instructions -->
-                <div class="p-4 rounded-md">
-                    <h2 class="text-green font-bold text-lg mb-4">Instructions</h2>
-                    <p class="text-black mb-16">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ac venenatis elit. Vivamus non urna ac turpis hendrerit tincidunt ut eget risus. Curabitur sagittis, ex a consectetur convallis, libero nisi efficitur sapien, non eleifend enim lectus vel leo. Morbi tincidunt libero ut nunc scelerisque, eget fringilla nulla volutpat. Aliquam feugiat massa sit amet arcu convallis, et iaculis ligula facilisis. Etiam accumsan magna et ipsum facilisis, at malesuada nulla ornare.</p>
-
-                    <div class="text-center">
-                        <a href="{{ url(MoreInstructions::getUrl()) }}" class="bg-orange text-white py-2 px-6 rounded-full hover-effect">
-                            FIND OUT MORE
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </div>
-
         <!-- Main Section -->
         <div class="grid md:grid-cols-4 gap-8">
 
