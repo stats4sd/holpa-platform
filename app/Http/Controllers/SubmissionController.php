7196a065183c8030163ce518974eb9bca80f9f8e
Dan Tang <dan@stats4sd.org>
2024-12-17 22:12:22 +0000

    SubmissionController, refactor custom handling for irrigation data

--- app/Http/Controllers/SubmissionController.php
+++ app/Http/Controllers/SubmissionController.php
@@ -18,46 +18,8 @@ class SubmissionController extends Controller
 
         // application specific business logic goes here
 
-        // TODO: fill in irrigation percentage for 12 months in farm_survey_data record
-
-        // initialise irrigation result, assume zero percentage irrigation for all months at the beginning
-        $irrigationResult = [];
-
-        for ($i = 1; $i <= 12; $i++) {
-            $irrigationResult['irrigation_percentage_month_' . $i] = 0;
-        }
-
-        // ray('*** initialisation ***');
-        // ray($irrigationResult);
-
-        if (isset($submission->content['survey']['income']['water']['irrigation_season_repeat'])) {
-            // get irrigation data from submission JSON content
-            $irrigations = $submission->content['survey']['income']['water']['irrigation_season_repeat'];
-
-            // ray('*** irrigation data ***');
-            // ray($irrigations);
-
-            // handle irrigation repeat groups one by one, overwrite percentage for a month if it has higher percentage
-            foreach ($irrigations as $irrigation) {
-                $irrigationResult = SubmissionController::prepareIrrigationData($irrigationResult, $irrigation);
-
-                // ray('*** handling repeat group ***');
-                // ray($irrigationResult);
-            }
-
-            ray('*** finalised irrigation result ***');
-            ray($irrigationResult);
-        }
-
-        $farmSurveyData = FarmSurveyData::where('submission_id', $submission->id)->first();
-
-        for ($i = 1; $i <= 12; $i++) {
-            $farmSurveyData['irrigation_percentage_month_' . $i] = $irrigationResult['irrigation_percentage_month_' . $i];
-        }
-
-        $farmSurveyData->save();
-
-        return;
+        // custom handling to fill in 12 months irrigation percentage in farm_survey_data record
+        SubmissionController::handleIrrigationData($submission);
 
 
         // TODO: create records from nested repeat groups for seasonal_worker_seasons, livestock_uses and fish_uses
@@ -69,12 +31,83 @@ class SubmissionController extends Controller
         // TODO: farms table, update column household_form_completed, fieldwork_form_completed
 
 
-        // TODO: update array location of "location" section
+        return;
 
+
+        // custom handling to create new locations
+        SubmissionController::handleLocationData($submission);
+    }
+
+
+
+    // custom handling for irrigation data
+    // to fill in irrigation percentage for 12 months in farm_survey_data record
+    public static function handleIrrigationData(Submission $submission): void
+    {
+        // initialise irrigation result, assume zero percentage irrigation for all months at the beginning
+        $irrigationResult = [];
+
+        for ($i = 1; $i <= 12; $i++) {
+            $irrigationResult['irrigation_percentage_month_' . $i] = 0;
+        }
+
+        // existence check for irrigation data in submission content
+        if (isset($submission->content['survey']['income']['water']['irrigation_season_repeat'])) {
+
+            // get irrigation data from submission JSON content
+            $irrigations = $submission->content['survey']['income']['water']['irrigation_season_repeat'];
+
+            // handle irrigation repeat groups one by one, overwrite percentage for a month if it has higher percentage
+            foreach ($irrigations as $irrigation) {
+                $irrigationResult = SubmissionController::prepareIrrigationData($irrigationResult, $irrigation);
+            }
+        }
+
+        // suppose there should be only one farm_survey_data for a submission id
+        $farmSurveyData = FarmSurveyData::where('submission_id', $submission->id)->first();
+
+        // update irrigation data for 12 months
+        for ($i = 1; $i <= 12; $i++) {
+            $farmSurveyData['irrigation_percentage_month_' . $i] = $irrigationResult['irrigation_percentage_month_' . $i];
+        }
+
+        // save farm_survey_data record
+        $farmSurveyData->save();
+    }
+
+
+    public static function prepareIrrigationData($irrigationResult, $irrigation): array
+    {
+        $irrigationPercentage = $irrigation['irrigation_percentage'];
+
+        // do nothing if irrigation_percentage is null or "null"
+        if ($irrigationPercentage == null || $irrigationPercentage == "null") {
+            ray('irrigation_percentage is null, do nothing');
+            return $irrigationResult;
+        }
+
+        $irrigationMonths = str_getcsv($irrigation['irrigation_months'], ' ');
+
+        foreach ($irrigationMonths as $irrigationMonth) {
+            if ($irrigationPercentage > $irrigationResult['irrigation_percentage_month_' . $irrigationMonth]) {
+                $irrigationResult['irrigation_percentage_month_' . $irrigationMonth] = $irrigationPercentage;
+            }
+        }
+
+        return $irrigationResult;
+    }
+
+
+    // custom handling for location
+    // to create new locations
+    // TODO: this code segment is tested with location selection test form, not yet tested with Fieldwork form and Household form
+    public static function handleLocationData(Submission $submission): void
+    {
         // find owner (team) of this submission
         $team = $submission->xlsformVersion->xlsform->owner;
 
         // get locations array
+        // TODO: update array location of "location" section in Fieldwork form and Household form
         $locations = $submission->content['location']['location_levels_rpt'];
 
         $parentLocationId = null;
@@ -132,27 +165,5 @@ class SubmissionController extends Controller
                 ]);
             }
         }
-    }
-
-
-    public static function prepareIrrigationData($irrigationResult, $irrigation): array
-    {
-        $irrigationPercentage = $irrigation['irrigation_percentage'];
-
-        // do nothing if irrigation_percentage is null or "null"
-        if ($irrigationPercentage == null || $irrigationPercentage == "null") {
-            ray('irrigation_percentage is null, do nothing');
-            return $irrigationResult;
-        }
-
-        $irrigationMonths = str_getcsv($irrigation['irrigation_months'], ' ');
-
-        foreach ($irrigationMonths as $irrigationMonth) {
-            if ($irrigationPercentage > $irrigationResult['irrigation_percentage_month_' . $irrigationMonth]) {
-                $irrigationResult['irrigation_percentage_month_' . $irrigationMonth] = $irrigationPercentage;
-            }
-        }
-
-        return $irrigationResult;
     }
 }
