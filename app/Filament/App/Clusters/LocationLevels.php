4e435238c407f3a881bc95ced0cca8307aa0e4ad
Ciara <ciara@stats4sd.org>
2024-12-18 17:03:17 +0000

    updates cluster

--- app/Filament/App/Clusters/LocationLevels.php
+++ app/Filament/App/Clusters/LocationLevels.php
@@ -7,4 +7,9 @@ use Filament\Clusters\Cluster;
 class LocationLevels extends Cluster
 {
     protected static ?string $navigationIcon = 'heroicon-o-squares-2x2';
+
+    public static function shouldRegisterNavigation():bool {
+         return false;
+    } 
+
 }
--- app/Filament/App/Clusters/LocationLevels/Resources/LocationLevelResource.php
+++ app/Filament/App/Clusters/LocationLevels/Resources/LocationLevelResource.php
@@ -2,30 +2,31 @@
 
 namespace App\Filament\App\Clusters\LocationLevels\Resources;
 
-use App\Filament\App\Clusters\LocationLevels\Resources\LocationLevelResource\Pages;
-use App\Filament\App\Clusters\LocationLevels\Resources\LocationLevelResource\RelationManagers\LocationsRelationManager;
-use App\Models\SampleFrame\LocationLevel;
+use Filament\Tables;
+use Filament\Forms\Form;
+use Filament\Tables\Table;
+use Illuminate\Support\Str;
 use App\Services\HelperService;
+use Filament\Infolists\Infolist;
+use Filament\Resources\Resource;
 use Filament\Forms\Components\Hidden;
 use Filament\Forms\Components\Select;
-use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Toggle;
-use Filament\Forms\Form;
-use Filament\Infolists\Components\TextEntry;
-use Filament\Infolists\Infolist;
 use Filament\Navigation\NavigationItem;
-use Filament\Resources\Resource;
-use Filament\Tables;
-use Filament\Tables\Table;
+use Filament\Forms\Components\TextInput;
+use App\Models\SampleFrame\LocationLevel;
 use Illuminate\Contracts\Support\Htmlable;
-use Illuminate\Support\Str;
+use Filament\Infolists\Components\TextEntry;
+use App\Filament\App\Clusters\LocationLevels;
+use App\Filament\App\Clusters\LocationLevels\Resources\FarmResource;
+use App\Filament\App\Clusters\LocationLevels\Resources\LocationLevelResource\Pages;
+use App\Filament\App\Clusters\LocationLevels\Resources\LocationLevelResource\RelationManagers\LocationsRelationManager;
 
 class LocationLevelResource extends Resource
 {
     protected static ?string $model = LocationLevel::class;
     protected static ?string $tenantOwnershipRelationshipName = 'owner';
-    
-    // protected static bool $shouldRegisterNavigation = false;
+    protected static ?string $cluster = LocationLevels::class;
 
     public static function getNavigationItems(): array
     {
@@ -41,11 +42,19 @@ class LocationLevelResource extends Resource
             ->map(function ($level) use ($baseRoute) {
                 return NavigationItem::make(Str::plural($level->name))
                     ->url($baseRoute . '/' . $level->slug)
-                    ->group('Survey Sample Frame')
-                    ->isActiveWhen(fn () => request()->routeIs(static::getRouteBaseName() . '.view') && request()->route('record') === $level->slug);
+                    ->isActiveWhen(function () use ($level) {
+                        $isViewRoute = request()->routeIs(static::getRouteBaseName() . '.view');
+                        $isMatchingRecord = request()->route('record') === $level->slug;
+
+                        return $isViewRoute && $isMatchingRecord;
+                    });
             });
 
-        return array_merge($original, $navItems->toArray());
+        $farmNavItem = NavigationItem::make('Farms')
+            ->url(FarmResource::getUrl())
+            ->isActiveWhen(fn () => request()->routeIs(FarmResource::getRouteBaseName() . '.index'));
+
+        return array_merge($original, $navItems->toArray(), [$farmNavItem]);
     }
 
     public static function form(Form $form): Form
