8a912e5b00f7fa43abf90f780b039856e08f066a
Dave Mills <d.e.mills@stats4sd.org>
2024-12-19 15:16:50 +0000

    WIP

--- resources/views/filament/app/components/place-locations-top-menu.blade.php
+++ resources/views/filament/app/components/place-locations-top-menu.blade.php
@@ -1,11 +1,24 @@
-<div class="mt-8 flex mx-auto space-x-4">
-    <a href="{{\App\Filament\App\Pages\TimeFrame::getUrl() }}">
-        Time Frame
-    </a>
-    <a href="{{ \App\Filament\App\Pages\DietDiversity::getUrl() }}">
-        Diet Diversity
-    </a>
-    <a href="{{ \App\Filament\App\Clusters\Localisations\Resources\ChoiceListEntryResource::getUrl('index') }}">
-        Choice Lists
-    </a>
-</div>
+<div class="mx-auto">
+    <div class="surveyblocks pb-24 mb-32 pt-12 px-12">
+        <div class="grid grid-cols-3 gap-4 h-max mb-12">
+            <a class="{{ request()->url() === \App\Filament\App\Pages\TimeFrame::getUrl() ? 'tabbuttons' : '' }}  rounded-2xl cursor-pointer lisptabs bg-gray-100 " href="{{\App\Filament\App\Pages\TimeFrame::getUrl() }}">
+                <livewire:rounded-square
+                    heading="Time Frame"
+                    description="Define the time frame for many recall questions."
+                />
+            </a>
+
+            <a class="{{ request()->url() === \App\Filament\App\Pages\DietDiversity::getUrl() ? 'tabbuttons' : '' }}  rounded-2xl cursor-pointer lisptabs bg-gray-100" href="{{ \App\Filament\App\Pages\DietDiversity::getUrl() }}">
+                <livewire:rounded-square
+                    heading="Diet Quality"
+                    description="Select the Diet Quality module for your country / context."
+                />
+            </a>
+            <a class="{{ request()->url() === App\Filament\App\Clusters\Localisations\Resources\ChoiceListEntryResource::getUrl('index') ? 'tabbuttons' : '' }}  rounded-2xl cursor-pointer lisptabs bg-gray-100" href="{{ \App\Filament\App\Clusters\Localisations\Resources\ChoiceListEntryResource::getUrl('index') }}">
+                <livewire:rounded-square
+                    heading="Choice List Entries"
+                    description="Customise the entries for units, crops, and other select questions"
+                />
+            </a>
+        </div>
+    </div>
