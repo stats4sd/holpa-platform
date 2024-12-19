dccae10615e743dafb940a5cef9d0dcd4e3f1909
Ciara <ciara@stats4sd.org>
2024-12-19 18:16:49 +0000

    removes in progress status when not needed

--- resources/views/filament/app/pages/survey-dashboard.blade.php
+++ resources/views/filament/app/pages/survey-dashboard.blade.php
@@ -261,13 +261,6 @@ use App\Filament\App\Pages\DataAnalysis;
                                         </svg>
                                         <span class="ml-1 inline text-xs font-semibold">NOT STARTED</span>
                                     </div>
-                                @elseif ($team->pilot_progress === 'in_progress')
-                                    <div class="mb-6">
-                                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 inline" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                                            <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125" />
-                                        </svg>
-                                        <span class="ml-1 inline text-xs font-semibold">IN PROGRESS</span>
-                                    </div>
                                 @elseif ($team->pilot_progress === 'complete')
                                     <div class="mb-6">
                                         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 inline" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
@@ -297,10 +290,6 @@ use App\Filament\App\Pages\DataAnalysis;
                             @if ($team->data_collection_progress === 'complete')
                                 <div class="w-3/4 bg-white bg-opacity-50 rounded-full h-2.5 mt-8 lg:mx-auto">
                                     <div class="bg-white h-2.5 rounded-full w-full" ></div>
-                                </div>
-                            @elseif ($team->data_collection_progress === 'in_progress')
-                                <div class="w-3/4 bg-white bg-opacity-50 rounded-full h-2.5 mt-8 lg:mx-auto">
-                                    <div class="bg-white h-2.5 rounded-full w-6/12" ></div>
                                 </div>
                             @else
                                 <div class="w-3/4 bg-white bg-opacity-50 rounded-full h-2.5 mt-8 lg:mx-auto">
@@ -358,10 +347,6 @@ use App\Filament\App\Pages\DataAnalysis;
                             @if ($team->data_analysis_progress === 'complete')
                                 <div class="w-3/4 bg-white bg-opacity-50 rounded-full h-2.5 mt-8 lg:mx-auto">
                                     <div class="bg-white h-2.5 rounded-full w-full" ></div>
-                                </div>
-                            @elseif ($team->data_analysis_progress === 'in_progress')
-                                <div class="w-3/4 bg-white bg-opacity-50 rounded-full h-2.5 mt-8 lg:mx-auto">
-                                    <div class="bg-white h-2.5 rounded-full w-6/12" ></div>
                                 </div>
                             @else
                                 <div class="w-3/4 bg-white bg-opacity-50 rounded-full h-2.5 mt-8 lg:mx-auto">
