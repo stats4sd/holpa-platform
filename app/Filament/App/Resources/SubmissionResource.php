7b1782d3a3ae3e67629ee8372b5e0484a0c04d09
Dave Mills <d.e.mills@stats4sd.org>
2024-12-20 09:17:46 +0000

    add draft global scope

---
+++ app/Filament/App/Resources/SubmissionResource.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Filament\App\Resources;
+
+use App\Filament\App\Resources\SubmissionResource\Pages;
+use App\Filament\App\Resources\SubmissionResource\RelationManagers;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Stats4sd\FilamentOdkLink\Models\OdkLink\Submission;
+
+class SubmissionResource extends Resource
+{
+    protected static ?string $model = Submission::class;
+
+    protected static bool $shouldRegisterNavigation = false;
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListSubmissions::route('/'),
+        ];
+    }
+}
