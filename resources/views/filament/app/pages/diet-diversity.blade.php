b780caa10e7d9b3617427a380f496546d81794d0
Emily <emily@stats4sd.org>
2024-12-20 08:38:21 +0000

    tweaks to localisation pages

--- resources/views/filament/app/pages/diet-diversity.blade.php
+++ resources/views/filament/app/pages/diet-diversity.blade.php
@@ -1,12 +1,15 @@
 <x-filament-panels::page>
-
+<div class="container mx-auto  ">
+        <div class="surveyblocks pt-16 pb-24 mb-32 px-12 lg:px-16">
     <p>HOLPA uses an internationally validated indicator for "dietary diversity". The questions in this section ask whether members of the household have consumed anything from specific food groups within the last 24 hours, such as grain food, tubers, pulses, green veg, etc. The default survey has all the needed questions, but does not include lists of locally contextualised example foods for each group.</p>
     <p>The << name of website with link>> site includes localised versions of these questions, with relevant example foods for each category for over 100 countries. This platform includes these localised modules, so you can select the option that fits your context below.</p>
-    <p>It is assumed that you are conducting HOLPA within one country. If you are working across multiple countries, we recommend creating a different "team" for each country so that you can make different customisations to the survey within the different countries.</p>
+    <p class="mb-6">It is assumed that you are conducting HOLPA within one country. If you are working across multiple countries, we recommend creating a different "team" for each country so that you can make different customisations to the survey within the different countries.</p>
 
     {{ $this->form }}
+    <div class="h-12"></div>
 
-
-    <p>Below are the questions that form this module. When you select your country, this table will update to show the text that will appear in your version of the survey.</p>
+    <p class="mb-6">Below are the questions that form this module. When you select your country, this table will update to show the text that will appear in your version of the survey.</p>
     {{ $this->table }}
+</div></div>
+</div>
 </x-filament-panels::page>
