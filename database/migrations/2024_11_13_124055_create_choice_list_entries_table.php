bf7564e33e4d4299d405f5abc5331e4eda363595
Dave Mills <d.e.mills@stats4sd.org>
2025-01-03 10:42:55 +0000

    WIP

--- database/migrations/2024_11_13_124055_create_choice_list_entries_table.php
+++ database/migrations/2024_11_13_124055_create_choice_list_entries_table.php
@@ -11,16 +11,16 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('choice_list_entries', function (Blueprint $table) {
-            $table->id();
-            $table->foreignId('choice_list_id')->constrained()->onDelete('cascade');
-
-            // entries might be global; or localised (localised ones have an owner)
-            $table->nullableMorphs('owner');
-            $table->string('name');
-            $table->json('properties')->nullable();
-            $table->timestamps();
-        });
+//        Schema::create('choice_list_entries', function (Blueprint $table) {
+//            $table->id();
+//            $table->foreignId('choice_list_id')->constrained()->onDelete('cascade');
+//
+//            // entries might be global; or localised (localised ones have an owner)
+//            $table->nullableMorphs('owner');
+//            $table->string('name');
+//            $table->json('properties')->nullable();
+//            $table->timestamps();
+//        });
     }
 
     /**
