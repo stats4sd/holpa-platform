7ed6bf3e07d99ab523df0bd8912f5fb8a6b14015
Dave Mills <d.e.mills@stats4sd.org>
2025-02-20 16:51:28 +0000

    hotfix - remove auto-generated seeder calls

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,24 +23,7 @@ class DatabaseSeeder extends Seeder
         foreach (glob(database_path('seeders/Prep/*.php')) as $file) {
             $class = 'Database\\Seeders\\Prep\\' . pathinfo($file, PATHINFO_FILENAME);
             $this->call($class);
-            $this->call(AppUsersTableSeeder::class);
-        $this->call(ChoiceListEntriesTableSeeder::class);
-        $this->call(ChoiceListsTableSeeder::class);
-        $this->call(DatasetsTableSeeder::class);
-        $this->call(LanguageStringsTableSeeder::class);
-        $this->call(LocalesTableSeeder::class);
-        $this->call(MediaTableSeeder::class);
-        $this->call(OdkProjectsTableSeeder::class);
-        $this->call(RequiredMediaTableSeeder::class);
-        $this->call(SurveyRowsTableSeeder::class);
-        $this->call(XlsformModulesTableSeeder::class);
-        $this->call(XlsformModuleVersionLocaleTableSeeder::class);
-        $this->call(XlsformModuleVersionsTableSeeder::class);
-        $this->call(XlsformsTableSeeder::class);
-        $this->call(XlsformTemplateSectionsTableSeeder::class);
-        $this->call(XlsformTemplatesTableSeeder::class);
-        $this->call(XlsformVersionsTableSeeder::class);
-    }
+        }
 
         // Call the test seeders locally
         if (app()->environment('local')) {
