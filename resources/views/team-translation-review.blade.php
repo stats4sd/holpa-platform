47ea809e520be0a9920cce44b1622cfc20f8c044
Dave Mills <d.e.mills@stats4sd.org>
2025-03-04 11:45:08 +0000

    hide 'created by another team' message if it was, in fact, created by the current team

--- resources/views/team-translation-review.blade.php
+++ resources/views/team-translation-review.blade.php
@@ -3,7 +3,7 @@
 
     @if($locale->is_default)
         <p>This translation is from the original ODK Forms, and cannot be directly edited. If you wish to create your own version of {{ $locale->language->name }}, you may add a new blank translation or duplicate this one and edit it.</p>
-    @elseif($locale->creator !== \Stats4sd\FilamentOdkLink\Services\HelperService::getCurrentOwner())
+    @elseif($locale->creator->id !== \Stats4sd\FilamentOdkLink\Services\HelperService::getCurrentOwner()->id)
         <p>This translation was uploaded by another team as a contribution to HOLPA. You cannot edit this translation directly, but you may duplicate it and edit your version if you wish.</p>
     @else
         <p>To edit this translation, click edit below. To keep this version as-is and create a new version to edit, click duplicate.</p>
