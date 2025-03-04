26d9cdc4a5482c59e4128edff08fbe447bc5c578
Dan Tang <dan@stats4sd.org>
2025-03-04 15:13:21 +0000

    Update dashboard error message content if a team does not have any xlsform

--- resources/views/filament/app/pages/info-panels/survey-dashboard.blade.php
+++ resources/views/filament/app/pages/info-panels/survey-dashboard.blade.php
@@ -8,7 +8,7 @@
 
         @if (auth()->user()->latestTeam->xlsforms()->count() == 0)
         <!-- Error message -->
-        <h3 class="text-white text-center mb-12 bg-blue">THIS TEAM DOES NOT HAVE ANY XLSFORM. PLEASE CONTACT SYSTEM ADMINISTRATOR TO TAKE CORRECTIVE ACTIONS.</h3>
+        <h3 class="text-white text-center mb-12 bg-blue">THIS TEAM DOES NOT HAVE ANY ODK FORMS. THIS IS DUE TO AN ISSUE WITH THE TEAM CREATION. PLEASE CONTACT {{ config('mail.to.support') }} FOR SUPPORT BEFORE CONTINUING.</h3>
         @endif
 
         <!-- Content -->
