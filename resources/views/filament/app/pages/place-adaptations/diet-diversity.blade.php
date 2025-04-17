7f152625ebfac85f1835b91d2ce21ad61685a243
Dave Mills <d.e.mills@stats4sd.org>
2025-04-17 13:50:50 +0100

    add more refs to external site

--- resources/views/filament/app/pages/place-adaptations/diet-diversity.blade.php
+++ resources/views/filament/app/pages/place-adaptations/diet-diversity.blade.php
@@ -2,10 +2,6 @@
     <x-instructions-sidebar>
         <x-slot:heading>Instructions</x-slot:heading>
         <x-slot:instructions>
-
-            {{-- <div class="pr-4 content-center  mx-auto my-4">
-                <iframe class="rounded-3xl" src="https://www.youtube.com/embed/TODO_ADD_VIDEO_ID" style="width: 560px; height: 315px;" frameborder="0" allowfullscreen></iframe>
-            </div> --}}
             <div class="mx-12 mb-4">
 
                 <h5>Diet Diversity module </h5>
@@ -23,8 +19,10 @@
 
     <div class="container mx-auto  ">
         <div class="surveyblocks pt-16 pb-24 mb-32 px-12 lg:px-16">
-            <p>HOLPA uses an internationally validated indicator for "dietary diversity". The questions in this section ask whether members of the household have consumed anything from specific food groups within the last 24 hours, such as grain food, tubers, pulses, green veg, etc. The default survey has all the needed questions, but does not include lists of locally contextualised example foods for each group.</p>
-            <p>The << name of website with link>> site includes localised versions of these questions, with relevant example foods for each category for over 100 countries. This platform includes these localised modules, so you can select the option that fits your context below.</p>
+            <p class="mb-4">HOLPA uses an internationally validated indicator for "dietary diversity". This indicator has a pre-defined set of questions, which is different for different countries. More information about this module can be found on the
+                <a href="https://www.dietquality.org/tools" class="text-green font-semibold">Global Diet Quality Project</a> website.
+            </p>
+            <p class="mb-6">To complete this section of the localisation, please select the version of the questions that you would like to use for your survey. If an option for your country does not exist, you can either use the default version (without locally-relevant food examples), or contact the HOLPA team to ask for your country's module to be added.</p>
             <p class="mb-6">It is assumed that you are conducting HOLPA within one country. If you are working across multiple countries, we recommend creating a different "team" for each country so that you can make different customisations to the survey within the different countries.</p>
 
             {{ $this->form }}
