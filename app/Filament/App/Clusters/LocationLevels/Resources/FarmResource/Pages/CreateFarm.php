89306e0fe9ee9bdaf643316d76a60ea5097c3f11
Dan Tang <dan@stats4sd.org>
2025-04-25 19:04:21 +0100

    WIP: copy import location levels popup model and import farms popup modal to a new page for importing both location levels and farms

--- app/Filament/App/Clusters/LocationLevels/Resources/FarmResource/Pages/CreateFarm.php
+++ app/Filament/App/Clusters/LocationLevels/Resources/FarmResource/Pages/CreateFarm.php
@@ -4,52 +4,91 @@ namespace App\Filament\App\Clusters\LocationLevels\Resources\FarmResource\Pages;
 
 use Filament\Forms\Get;
 use App\Services\HelperService;
-
 use Filament\Forms\Components\Hidden;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\Section;
 use App\Models\SampleFrame\LocationLevel;
-
 use Filament\Forms\Components\FileUpload;
 use Filament\Forms\Components\Wizard\Step;
 use Filament\Resources\Pages\CreateRecord;
+use Filament\Forms\Components\CheckboxList;
+use Filament\Tables\Contracts\HasTable;
+use Illuminate\Support\Facades\Storage;
+use Livewire\Features\SupportFileUploads\TemporaryUploadedFile;
 use Illuminate\Http\Client\RequestException;
-
 use Stats4sd\FilamentOdkLink\Models\OdkLink\Platform;
 use Stats4sd\FilamentOdkLink\Services\OdkLinkService;
 use Stats4sd\FilamentOdkLink\Jobs\UpdateXlsformTitleInFile;
 use Stats4sd\FilamentOdkLink\Models\OdkLink\XlsformTemplate;
 use Illuminate\Contracts\Container\BindingResolutionException;
 use App\Filament\App\Clusters\LocationLevels\Resources\FarmResource;
-use Stats4sd\FilamentOdkLink\Filament\OdkAdmin\Resources\XlsformTemplateResource;
-
 
 class CreateFarm extends CreateRecord
 {
+    use CreateRecord\Concerns\HasWizard;
+
     protected static string $resource = FarmResource::class;
 
-    use CreateRecord\Concerns\HasWizard;
+    // Code segment belongs to superclass ExcelImportAction starts here...
+
+    protected ?string $disk = null;
+
+    protected function getDisk()
+    {
+        return $this->disk ?: config('filesystems.default');
+    }
+
+    // Code segment belongs to superclass ExcelImportAction ends here...
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this
+            ->modalWidth('3xl')
+            ->modalDescription('Import Farms from an Excel file. The first worksheet of the Excel file should contain the data to import. The first row of the worksheet should contain the column headings. You must have already created or imported the locations that the farms will be associated with.');
+    }
+
 
     public function getSteps(): array
     {
         return [
-            Step::make('1. Xlsform')
-                ->description('Upload your XLSForm file and give it a title')
+
+            // Step 1
+            Step::make('Upload your farm list excel file')
+                ->schema([
+
+                    // Question: is the file upload works for ExcelImportAction's subclass only?
+                    // FileUpload::make('upload')
+                    //     ->label(fn($livewire) => str($livewire->getRecord()->name)->plural()->title() . ' ' . 'Excel Data')
+                    //     ->disk($this->getDisk())
+                    //     ->columns()
+                    //     ->required()
+                    //     ->live()
+                    //     ->afterStateUpdated(function (?TemporaryUploadedFile $state, Set $set) {
+                    //         $headings = (new HeadingRowImport)->toArray($state->getRealPath());
+
+                    //         // $headings is an array(sheets) of arrays(headers)
+                    //         // We only want the first sheet
+                    //         $headings = $headings[0][0];
+
+                    //         $set('header_columns', $headings ?? []);
+                    //     }),
+
+                ]),
+
+
+            // Step 2
+            Step::make('Map columns to location levels')
                 ->schema(
-                    // XlsformTemplateResource::getCreateFields(),
 
                     [
                         Section::make('Column Mapping')
                             ->columns(2)
                             ->schema(function ($livewire) {
-                                // TODO: add file upload compoenent
 
-
-
-                                // find the location level that has farms
-                                // $currentLevel = $livewire->getRecord();
-                                $currentLevel = LocationLevel::where('has_farms', true)->first();
-
+                                $hasFarmLevel = LocationLevel::where('has_farms', 1)->first();
+                                $currentLevel = $hasFarmLevel;
                                 $parents = collect([]);
 
                                 while ($currentLevel->parent) {
@@ -58,8 +97,6 @@ class CreateFarm extends CreateRecord
                                 }
 
                                 $parentQuestions = $parents->reverse()->map(callback: function ($parent) {
-                                    ray($parent->name);
-
                                     return collect([
                                         Select::make("parent_{$parent->id}_code_column")
                                             ->label(fn($livewire) => "Which column contains the {$parent->name} unique code?")
@@ -74,17 +111,14 @@ class CreateFarm extends CreateRecord
                                     ]);
                                 })->flatten();
 
-                                // Question: why current level is District? It should be Village
-                                ray($currentLevel->name);
-
                                 $currentLevelQuestions = collect([
                                     Select::make('code_column')
-                                        ->label(fn($livewire) => "Which column contains the {$currentLevel->name} unique code?")
+                                        ->label(fn($livewire) => "Which column contains the {$hasFarmLevel->name} unique code?")
                                         ->options(fn(Get $get) => $get('header_columns'))
                                         ->notIn(['na'])
                                         ->required(),
                                     Select::make('name_column')
-                                        ->label(fn($livewire) => "Which column contains the {$currentLevel->name} name?")
+                                        ->label(fn($livewire) => "Which column contains the {$hasFarmLevel->name} name?")
                                         ->options(fn(Get $get) => $get('header_columns'))
                                         ->notIn(['na'])
                                         ->required(),
@@ -106,7 +140,7 @@ class CreateFarm extends CreateRecord
                             ->default(['na' => '~~upload a file to see the headers~~'])
                             ->live(),
                         Hidden::make('level')
-                            ->default(fn($livewire) => $livewire->getRecord()),
+                            ->default(LocationLevel::where('has_farms', 0)->first()),
                         Hidden::make('user_id')
                             ->default(fn() => auth()->id()),
                         Hidden::make('owner_id')
@@ -131,15 +165,76 @@ class CreateFarm extends CreateRecord
                     return redirect($this->getResource()::getUrl('edit', ['record' => $xlsformTemplate]));
                 }),
 
-            Step::make('2. Add Media Files')
-                ->description('Add any static media required by the form')
-                ->schema([]),
-            Step::make('3. Link Required Datasets')
-                ->description('Add / link external datasets for lookup tables')
-                ->schema([]),
-            Step::make('4. Review Xlsform Structure')
-                ->description('How should the collected data be handled?')
-                ->schema([]),
+
+            // Step 3
+            Step::make('Map columns to farm')
+                ->schema([
+
+                    Hidden::make('header_columns')
+                        ->default(['na' => '~~upload a file to see the column headers~~'])
+                        ->live(),
+
+                    Section::make('Location')
+                        ->schema([
+
+                            // Question: is it possible to have more than one location levels have farms?
+                            // can we set default value to simplify the import process?
+                            Select::make('location_level_id')
+                                ->label('Which location level are the farms linked to?')
+                                ->options(
+                                    LocationLevel::where('has_farms', true)->get()->pluck('name', 'id')
+                                )
+                                ->placeholder('Select a location level')
+                                ->helperText('For many sampling strategies, this will be obvious (the lowest level. It may be less obvious when there are different hierarchies of locations in different places.')
+                                ->live(),
+
+                            Select::make('location_code_column')
+                                ->options(fn(Get $get) => $get('header_columns'))
+                                ->label(fn(Get $get) => 'Which column contains the ' . (LocationLevel::find($get('location_level_id'))?->name ?? 'location') . ' unique code?')
+                                ->placeholder('Select a column'),
+                        ]),
+
+                    Section::make('Farm Information')
+                        ->columns(1)
+                        ->schema([
+                            Select::make('farm_code_column')
+                                ->label('Which column contains the farm unique code?')
+                                ->placeholder('Select a column')
+                                ->helperText('e.g. farm_id or farm_code')
+                                ->live()
+                                ->options(fn(Get $get) => $get('header_columns')),
+
+                            CheckboxList::make('farm_identifiers')
+                                ->label('Are there any additional columns that contain identifiers for the farm? Tick all that apply.')
+                                ->helperText('For example: family name, farm name, telephone numbers, etc. These are columns that can be useful for enumerators or project team members to identify the farm, but that should not be shared outside the project for data protection purposes.')
+                                ->options(fn(Get $get): array => $get('header_columns'))
+                                ->disableOptionWhen(
+                                    fn(string $value, Get $get): bool => $value === (string) $get('farm_code_column') ||
+                                        collect($get('farm_properties'))->contains($value) ||
+                                        $value === 'na'
+                                )
+                                ->live()
+                                ->columnSpanFull(),
+
+                            CheckboxList::make('farm_properties')
+                                ->label('Are there any additional columns that contain properties of the farm? Tick all that apply.')
+                                ->helperText('These are not identifiers, but are properties of the farm that are useful for analysis. For example: size of the farm, year of first engagement, etc. These are columns that can potentially be shared outside the project for analysis purposes.')
+                                ->options(fn(Get $get) => $get('header_columns'))
+                                ->disableOptionWhen(
+                                    fn(string $value, Get $get): bool => $value === (string) $get('farm_code_column') ||
+                                        collect($get('farm_identifiers'))->contains($value) ||
+                                        $value === 'na'
+                                )
+                                ->live()
+                                ->columnSpanFull(),
+
+                            Hidden::make('owner_id')
+                                ->default(HelperService::getCurrentOwner()->id),
+
+                        ]),
+
+                ]),
+
         ];
     }
 
@@ -155,27 +250,62 @@ class CreateFarm extends CreateRecord
         $this->processRecord($test);
     }
 
-    /**
-     * @throws RequestException
-     * @throws BindingResolutionException
-     */
-    protected function processRecord(XlsformTemplate $record): XlsformTemplate
-    {
-        $odkLinkService = app()->make(OdkLinkService::class);
+    // /**
+    //  * @throws RequestException
+    //  * @throws BindingResolutionException
+    //  */
+    // protected function processRecord(XlsformTemplate $record): XlsformTemplate
+    // {
+    //     $odkLinkService = app()->make(OdkLinkService::class);
 
-        $record->owner()->associate(Platform::first());
-        $record->saveQuietly();
+    //     $record->owner()->associate(Platform::first());
+    //     $record->saveQuietly();
 
-        // update form title in xlsfile to match user-given title
-        UpdateXlsformTitleInFile::dispatchSync($record);
+    //     // update form title in xlsfile to match user-given title
+    //     UpdateXlsformTitleInFile::dispatchSync($record);
 
-        $record->refresh();
-        $record->deployDraft($odkLinkService);
-        $record->getRequiredMedia($odkLinkService);
+    //     $record->refresh();
+    //     $record->deployDraft($odkLinkService);
+    //     $record->getRequiredMedia($odkLinkService);
 
-        // TODO: We need to do the extract section when create and edit
-        $record->extractSections();
+    //     // TODO: We need to do the extract section when create and edit
+    //     $record->extractSections();
 
-        return $record;
-    }
+    //     return $record;
+    // }
+
+    // public function action(Closure|string|null $action): static
+    // {
+    //     if ($action !== 'importData') {
+    //         throw new RuntimeException('You cannot override the action for this plugin');
+    //     }
+
+    //     $this->action = $this->importData();
+
+    //     return $this;
+    // }
+
+    // public function importData(): Closure
+    // {
+    //     return function (array $data, $livewire): bool {
+
+    //         // create import record - for review and error tracking by users
+    //         $import = Import::create([
+    //             'team_id' => HelperService::getCurrentOwner()->id,
+    //             'model_type' => Farm::class,
+    //         ]);
+
+    //         $import->addMedia(Storage::path($data['upload']))->toMediaCollection();
+
+    //         $data['import_id'] = $import->id;
+
+    //         // setup importer
+    //         $importObject = new $this->importClass($data);
+
+    //         // run import
+    //         Excel::import($importObject, $import->getFirstMediaPath());
+
+    //         return true;
+    //     };
+    // }
 }
