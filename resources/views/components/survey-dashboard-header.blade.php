4aaada403066e7a8bd147a46c834ebf21df62038
Emily <emily@stats4sd.org>
2025-04-29 11:37:52 +0100

    hedaer alignment tweaks

--- resources/views/components/survey-dashboard-header.blade.php
+++ resources/views/components/survey-dashboard-header.blade.php
@@ -18,11 +18,10 @@ use App\Filament\App\Pages\PlaceAdaptations\InitialPilot;
 <div class="relative inset-0 f h-full md:max-h-[80vh] lg:max-h-[70vh] bg-black bg-opacity-50 pt-6 lg:pt-2 z-10 px-8 md:px-24 py-20 pr-36">
 
         <!-- Headings -->
-
+<div class="container mx-auto xl:px-28">
                     <h2 class="text-white text-5xl mb-2 lg:mb-4 mt-20 font-extralight" style="letter-spacing: 0.3em;">HOLPA</h2>
                         <h1 class="text-hyellow text-4xl sm:text-5xl lg:text-6xl mb-2 lg:mb-4">Survey Dashboard</h1>
-
-        <!-- Explore Cards -->
+</div>
 
 
     </div>
