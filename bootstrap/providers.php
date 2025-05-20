d5a079cdb75f43766f5c61b00483587d2b43d447
Dave Mills <d.e.mills@stats4sd.org>
2025-05-20 16:09:48 +0100

    add horizon

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,9 +2,10 @@
 
 return [
     App\Providers\AppServiceProvider::class,
-        App\Providers\LivewireDiscoverServiceProvider::class,
     App\Providers\Filament\AdminPanelProvider::class,
     App\Providers\Filament\AppPanelProvider::class,
     App\Providers\Filament\ProgramPanelProvider::class,
+    App\Providers\HorizonServiceProvider::class,
+    App\Providers\LivewireDiscoverServiceProvider::class,
     App\Providers\TelescopeServiceProvider::class,
 ];
