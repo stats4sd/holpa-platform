3ef411a08819e1c3ae249b3083423f96b5aad229
Dave Mills <d.e.mills@stats4sd.org>
2025-06-19 10:56:51 +0100

    generalise creation of local modules for extending global modules

---
+++ app/Filament/App/Pages/SurveyLocations/ContextQuestions.php
@@ -0,0 +1,358 @@
+<?php
+
+namespace App\Filament\App\Pages\SurveyLocations;
+
+use App\Models\Team;
+use App\Services\HelperService;
+use Awcodes\TableRepeater\Components\TableRepeater;
+use Awcodes\TableRepeater\Header;
+use Filament\Actions\Concerns\InteractsWithActions;
+use Filament\Actions\Contracts\HasActions;
+use Filament\Actions\StaticAction;
+use Filament\Forms\Components\Fieldset;
+use Filament\Forms\Components\Hidden;
+use Filament\Forms\Components\Repeater;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Concerns\InteractsWithForms;
+use Filament\Forms\Contracts\HasForms;
+use Filament\Forms\Get;
+use Filament\Pages\Page;
+use Filament\Support\Enums\MaxWidth;
+use Filament\Tables\Actions\Action;
+use Filament\Tables\Actions\CreateAction;
+use Filament\Tables\Actions\DeleteAction;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Concerns\InteractsWithTable;
+use Filament\Tables\Contracts\HasTable;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+use Stats4sd\FilamentOdkLink\Models\OdkLink\SurveyRow;
+use Stats4sd\FilamentOdkLink\Models\OdkLink\XlsformLanguages\LanguageStringType;
+use Stats4sd\FilamentOdkLink\Models\OdkLink\XlsformLanguages\Locale;
+use Stats4sd\FilamentOdkLink\Models\OdkLink\XlsformModuleVersion;
+
+class ContextQuestions extends Page implements HasActions, HasForms, HasTable
+{
+
+    use InteractsWithActions;
+    use InteractsWithForms;
+    use InteractsWithTable;
+
+    protected static ?string $navigationIcon = 'heroicon-o-document-text';
+
+    protected static string $view = 'filament.app.pages.survey-locations.context-questions';
+
+    public Team|Model|null $team;
+    public XlsformModuleVersion $xlsformModuleVersion;
+
+    public function mount(): void
+    {
+        $this->team = HelperService::getCurrentOwner();
+
+        if ($this->team === null) {
+            abort(404);
+        }
+
+        $this->xlsformModuleVersion = $this->team->localContextModuleVersion->load(['surveyRows.languageStrings', 'surveyRows.choiceList.choiceListEntries.languageStrings']);
+
+        $this->form->fill($this->xlsformModuleVersion->toArray());
+    }
+
+// show questions in a table for better readability
+    public function table(Table $table): Table
+    {
+
+        $locales = $this->team->locales;
+
+        $choiceListHeaders = $locales->map(function (Locale $locale) {
+            return [
+                Header::make('label_' . $locale->id)->label('Label - ' . $locale->language_label),
+            ];
+        })->flatten()->toArray();
+
+        // for new questions, set the default languageStrings to 'label' + 'hint' - one for each of the team locales.
+        $defaultLanguageStringState = $locales->map(function (Locale $locale) {
+            return [
+                [
+                    'locale_id' => $locale->id,
+                    'language_string_type_id' => LanguageStringType::where('name', 'label')->first()->id,
+                    'text' => '',
+                ],
+                [
+                    'locale_id' => $locale->id,
+                    'language_string_type_id' => LanguageStringType::where('name', 'hint')->first()->id,
+                    'text' => '',
+                ],
+            ];
+        })->flatten(1)->toArray();
+
+        $defaultChoiceLanguageStringState = $locales->map(function (Locale $locale) {
+            return [
+                'locale_id' => $locale->id,
+                'language_string_type_id' => LanguageStringType::where('name', 'label')->first()->id,
+                'text' => '',
+            ];
+        })->toArray();
+
+        return $table
+            ->query(
+                fn() => SurveyRow::query()
+                    ->where('xlsform_module_version_id', $this->xlsformModuleVersion->id)
+                    // sort record by row_number, to reflect the user defined ordering by drag and drop
+                    ->orderBy('row_number'),
+            )
+            ->columns([
+                TextColumn::make('type')->label('Question Type'),
+                TextColumn::make('name')->label('Variable Name')->wrap(),
+                // fix to show default label as string instead of a JSON array
+                TextColumn::make('defaultLabel.text')->label('Default Label'),
+            ])
+            // allow user to change the ordering by drag and drop
+            ->reorderable('row_number')
+            ->reorderRecordsTriggerAction(
+                fn(Action $action, bool $isReordering) => $action
+                    ->button()
+                    ->label($isReordering ? 'Disable reordering' : 'Enable reordering')
+                    ->extraAttributes(['class' => 'buttona border-0 !ring-0 !shadow-none -ml-2  mb-4']))
+            ->paginated(false)
+
+            // add "create new record" button in table header
+            ->headerActions([
+                CreateAction::make()
+                    ->label('ADD QUESTION')
+                    ->icon('heroicon-m-plus')
+                    ->button()
+                    ->color('danger')
+                    ->extraAttributes(['class' => 'add_questions_btn'])
+                    ->modalWidth(MaxWidth::SevenExtraLarge)
+                    ->modalHeading('ADD QUESTION')
+                    ->extraModalWindowAttributes(['class' => 'add_questions_modal'])
+                    ->modalSubmitAction(fn(StaticAction $action) => $action
+                        ->extraAttributes(['class' => 'buttona shadow-none !ring-0 border-0']))
+                    ->modalCancelAction(fn(StaticAction $action) => $action
+                        ->extraAttributes(['class' => 'buttonb shadow-none !ring-0 ']))
+                    ->createAnother(false)
+                    ->mutateFormDataUsing(function (array $data): array {
+                        // find the largest row_number of survey_rows records
+                        $lastRowNumber = $this->xlsformModuleVersion->surveyRows->count() != 0 ? $this->xlsformModuleVersion->surveyRows->last()->row_number : 0;
+
+                        $data['row_number'] = $lastRowNumber + 1;
+
+                        return $data;
+                    })
+                    ->form([
+                        Hidden::make('xlsform_module_version_id')->default($this->xlsformModuleVersion->id)->live(),
+                        Hidden::make('id'),
+                        Fieldset::make('Question Information')
+                            ->columns([
+                                'sm' => 1,
+                                'md' => 2,
+                                'lg' => 2,
+                            ])
+                            ->schema([
+                                Select::make('type')->options([
+                                    'select_one' => 'select_one',
+                                    'select_multiple' => 'select_multiple',
+                                    'decimal' => 'decimal',
+                                    'integer' => 'integer',
+                                    'text' => 'text',
+                                ])->live()
+                                    ->required(),
+                                TextInput::make('name')->label('Variable Name')->live()->required()
+                                    ->dehydrateStateUsing(fn($state): string => Str::lower(Str::slug($state, '_'))),
+                                Repeater::make('languageStrings')
+                                    ->extraAttributes(['class' => 'inline-repeater'])
+                                    ->label('')
+                                    ->columnSpan(2)
+                                    ->addable(false)
+                                    ->deletable(false)
+                                    ->grid(2)
+                                    ->relationship('languageStrings')
+                                    ->saveRelationshipsWhenHidden(true)
+                                    ->schema([
+                                        Hidden::make('locale_id'),
+                                        Hidden::make('language_string_type_id'),
+
+                                        TextInput::make('text')
+                                            ->required()
+                                            ->label(fn(Get $get) => LanguageStringType::find($get('language_string_type_id'))->name . ' - ' . Locale::find($get('locale_id'))->language_label),
+                                    ])
+                                    ->default($defaultLanguageStringState)
+                                    ->live(),
+
+                            ])->live(),
+                        Fieldset::make('Choice List')
+                            ->columns([
+                                'sm' => 1,
+                                'md' => 1,
+                                'lg' => 1,
+                            ])
+                            ->live()
+                            ->relationship('choiceList')
+                            ->visible(fn(Get $get) => $get('type') === 'select_one' || $get('type') === 'select_multiple')
+                            ->schema([
+                                Hidden::make('xlsform_module_version_id')->default($this->xlsformModuleVersion->id)->live(),
+                                Hidden::make('list_name')->live()
+                                    ->formatStateUsing(fn(Get $get) => $get('../name') . '_choices'),
+                                TableRepeater::make('choiceListEntries')
+                                    ->relationship('choiceListEntries')
+                                    ->headers([
+                                        Header::make('name')->label('Name'),
+                                        ...$choiceListHeaders,
+                                    ])
+                                    ->schema([
+                                        TextInput::make('name')->required(),
+                                        Hidden::make('owner_id')->default(HelperService::getCurrentOwner()->id),
+                                        Repeater::make('languageStrings')
+                                            ->extraAttributes(['class' => 'inline-repeater'])
+                                            ->label('')
+                                            ->addable(false)
+                                            ->deletable(false)
+                                            ->relationship('languageStrings')
+                                            ->saveRelationshipsWhenHidden(true)
+                                            ->schema([
+                                                Hidden::make('locale_id'),
+                                                Hidden::make('language_string_type_id'),
+
+                                                TextInput::make('text')
+                                                    ->label(''),
+                                            ])
+                                            ->default($defaultChoiceLanguageStringState)
+                                            ->live(),
+                                    ]),
+                            ]),
+
+                    ]),
+            ])
+
+            // show form content in modal popup
+            ->actions([
+
+                Action::make('view_edit_question')
+                    ->label('VIEW/EDIT QUESTION')
+                    ->icon('heroicon-m-pencil')
+                    ->extraAttributes(['class' => 'py-2 shadow-none'])
+                    ->button()
+                    ->color('blue')
+                    // disable editing uploaded custom questions
+                    // Note: we use survey_rows.path to determine if a custom question is uploaded or not now,
+                    // when we start using survey_rows.path for matching submissions to SurveyRow entries,
+                    // we will need to create a new column as a flag for indication
+                    ->disabled(fn(SurveyRow $record) => $record->path != null)
+                    // set more horizontal space for modal popup
+                    ->modalWidth(MaxWidth::SevenExtraLarge)
+                    // fill the form with existing data
+                    // Question: why the changes of type and name cannot be saved into survey_rows record?
+                    ->fillForm(fn(SurveyRow $record): array => [
+                        'xlsform_module_version_id' => $this->xlsformModuleVersion->id,
+                        'id' => $record->id,
+                        'type' => $record->type,
+                        'name' => $record->name,
+                    ])
+                    ->form([
+                        Hidden::make('xlsform_module_version_id')->default($this->xlsformModuleVersion->id)->live(),
+                        Hidden::make('id'),
+                        Fieldset::make('Question Information')
+                            ->columns([
+                                'sm' => 1,
+                                'md' => 2,
+                                'lg' => 2,
+                            ])
+                            ->schema([
+                                Select::make('type')->options([
+                                    'select_one' => 'select_one',
+                                    'select_multiple' => 'select_multiple',
+                                    'decimal' => 'decimal',
+                                    'integer' => 'integer',
+                                    'text' => 'text',
+                                ])->live()
+                                    ->required(),
+                                TextInput::make('name')->label('Variable Name')->live()->required()
+                                    ->dehydrateStateUsing(fn($state): string => Str::lower(Str::slug($state, '_'))),
+                                Repeater::make('languageStrings')
+                                    ->extraAttributes(['class' => 'inline-repeater'])
+                                    ->label('')
+                                    ->columnSpan(2)
+                                    ->addable(false)
+                                    ->deletable(false)
+                                    ->grid(2)
+                                    ->relationship('languageStrings')
+                                    ->saveRelationshipsWhenHidden(true)
+                                    ->schema([
+                                        Hidden::make('locale_id'),
+                                        Hidden::make('language_string_type_id'),
+
+                                        TextInput::make('text')
+                                            ->required()
+                                            ->label(fn(Get $get) => LanguageStringType::find($get('language_string_type_id'))->name . ' - ' . Locale::find($get('locale_id'))->language_label),
+                                    ])
+                                    ->default($defaultLanguageStringState)
+                                    ->live(),
+
+                            ])->live(),
+                        Fieldset::make('Choice List')
+                            ->columns([
+                                'sm' => 1,
+                                'md' => 1,
+                                'lg' => 1,
+                            ])
+                            ->live()
+                            ->relationship('choiceList')
+                            ->visible(fn(Get $get) => $get('type') === 'select_one' || $get('type') === 'select_multiple')
+                            ->schema([
+                                Hidden::make('xlsform_module_version_id')->default($this->xlsformModuleVersion->id)->live(),
+                                Hidden::make('list_name')->live()
+                                    ->formatStateUsing(fn(Get $get) => $get('../name') . '_choices'),
+                                TableRepeater::make('choiceListEntries')
+                                    ->relationship('choiceListEntries')
+                                    ->headers([
+                                        Header::make('name')->label('Name'),
+                                        ...$choiceListHeaders,
+                                    ])
+                                    ->schema([
+                                        TextInput::make('name')->required(),
+                                        Hidden::make('owner_id')->default(HelperService::getCurrentOwner()->id),
+                                        Repeater::make('languageStrings')
+                                            ->extraAttributes(['class' => 'inline-repeater'])
+                                            ->label('')
+                                            ->addable(false)
+                                            ->deletable(false)
+                                            ->relationship('languageStrings')
+                                            ->saveRelationshipsWhenHidden(true)
+                                            ->schema([
+                                                Hidden::make('locale_id'),
+                                                Hidden::make('language_string_type_id'),
+
+                                                TextInput::make('text')
+                                                    ->label('test'),
+                                            ])
+                                            ->default($defaultChoiceLanguageStringState)
+                                            ->live(),
+                                    ]),
+                            ]),
+
+                    ])
+                    // save changes of type and name to survey_rows record after user clicking modal popup form "Submit" button
+                    ->action(function (array $data) {
+                        $surveyRow = SurveyRow::find($data['id']);
+
+                        $surveyRow->type = $data['type'];
+                        $surveyRow->name = $data['name'];
+
+                        $surveyRow->save();
+                    }),
+
+                // add "DELETE QUESTION" button in table row instead of inside modal popup
+                DeleteAction::make()
+                    ->label('DELETE QUESTION')
+                    ->extraAttributes(['class' => 'py-2 shadow-none'])
+                    ->button()
+                    ->modalHeading('Delete Question'),
+
+            ]);
+    }
+
+
+}
