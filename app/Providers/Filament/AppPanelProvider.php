d8235821e1771c0d8b3613f6335f25e6a957858e
Dan Tang <dan@stats4sd.org>
2025-10-30 09:30:48 +0000

    Submodule update - Register new team, set team name field as compulsory; change team name field label but failed

--- app/Providers/Filament/AppPanelProvider.php
+++ app/Providers/Filament/AppPanelProvider.php
@@ -40,8 +40,7 @@ class AppPanelProvider extends PanelProvider
             ->id('app')
             ->path('app')
             ->tenant(Team::class)
-            // disable "Register New Team" option in multi-tenancy
-            // new team should be created by admin, user should not be able to create a new team
+            // any user can create a new team
             ->tenantRegistration(RegisterTeam::class)
             ->tenantMiddleware([
                 SetLatestTeamMiddleware::class,
