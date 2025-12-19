b9941c8625b24134c1d77339b0d995e460c6b135
Dave Mills <d.e.mills@stats4sd.org>
2025-12-19 15:27:52 +0000

    fix case sensitivity

--- resources/js/public-map.js
+++ resources/js/public-map.js
@@ -3,7 +3,7 @@ console.log('public map js loaded');
 // create Vue app and mount to #public-map-app
 
 import { createApp } from 'vue';
-import PublicMap from './components/PublicMap.vue';
+import PublicMap from './Components/PublicMap.vue';
 
 document.documentElement.style.setProperty("--vs-menu-z-index", 2000);
 
